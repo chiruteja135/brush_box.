3ce5ebc5778b0024ca3addafff9a1b88920ecd6e
Chiruteja135 <chiruteja135@gmail.com>
2024-03-05 17:21:03 +0530

    final release

--- src/Services.js
+++ src/Services.js
@@ -6,20 +6,23 @@ import "./App.css";
 const servicesData = [
   {
     title: "Washing",
-    description: "Our classic service includes a thorough wash cycle followed by professional ironing for a crisp, polished finish. Perfect for everyday essentials like shirts, pants, and polos.",
+    description:
+      "Our classic service includes a thorough wash cycle followed by professional ironing for a crisp, polished finish. ",
   },
   {
     title: "Steam Ironing",
-    description: "This service focuses solely on professional ironing using a powerful steam iron for deep crease removal and a luxurious drape. Ideal for delicate fabrics, dress shirts, and formal wear.",
-  
+    description:
+      "This service focuses solely on professional ironing using a powerful steam iron for deep crease removal and a luxurious drape. ",
   },
   {
     title: "Washing & Fold",
-    description: "Our most convenient option! This service combines a thorough wash cycle with expert folding, delivering your clothes clean, crisp, and ready to wear.",
+    description:
+      "Our most convenient option! This service combines a thorough wash cycle with expert folding, delivering your clothes clean, crisp, and ready to wear.",
   },
   {
     title: "Folding",
-    description: "Skip the folding chore! We expertly fold and organize your clean laundry, saving you time and effort. Perfect for busy professionals, families, or anyone who enjoys having things perfectly stacked.",
+    description:
+      "Skip the folding chore! We expertly fold and organize your clean laundry, saving you time and effort.",
   },
 ];
 
@@ -31,7 +34,7 @@ const Services = () => {
           <h3>{service.title}</h3>
           {
             // Check if mobile screen using media query
-            window.innerWidth <= 768 ? null : ( <p>{service.description}</p>) // Render description for desktop Don't render description for mobile
+            window.innerWidth <= 768 ? null : <p>{service.description}</p> // Render description for desktop Don't render description for mobile
           }
         </div>
       ))}
